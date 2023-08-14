a365975b564e905a34b80e030e9e434ee1959588
Rob Skilling <rob@dxw.com>
2023-08-14 11:23:09 +0100

    Implement standard dxw PHP CS Fixer config

--- tests/use_atom_test.php
+++ tests/use_atom_test.php
@@ -4,80 +4,82 @@ use PHPUnit\Framework\TestCase;
 
 class UseAtom_Test extends TestCase
 {
-    public function setUp()
-    {
-        \WP_Mock::setUp();
-    }
+	public function setUp()
+	{
+		\WP_Mock::setUp();
+	}
 
-    public function tearDown()
-    {
-        \WP_Mock::tearDown();
-    }
+	public function tearDown()
+	{
+		\WP_Mock::tearDown();
+	}
 
-    public function testRegister()
-    {
-        $useAtom = new \Dxw\Iguana\Extras\UseAtom();
+	public function testRegister()
+	{
+		$useAtom = new \Dxw\Iguana\Extras\UseAtom();
 
-        $this->assertInstanceOf(\Dxw\Iguana\Registerable::class, $useAtom);
+		$this->assertInstanceOf(\Dxw\Iguana\Registerable::class, $useAtom);
 
-        \WP_Mock::expectActionAdded('init', [$useAtom, 'init']);
-        \WP_Mock::expectActionAdded('wp_head', [$useAtom, 'wpHead']);
+		\WP_Mock::expectActionAdded('init', [$useAtom, 'init']);
+		\WP_Mock::expectActionAdded('wp_head', [$useAtom, 'wpHead']);
 
-        $useAtom->register();
-    }
+		$useAtom->register();
+	}
 
-    public function testInit()
-    {
-        $useAtom = new \Dxw\Iguana\Extras\UseAtom();
+	public function testInit()
+	{
+		$useAtom = new \Dxw\Iguana\Extras\UseAtom();
 
-        \WP_Mock::expectFilterAdded('default_feed', [$useAtom, 'defaultFeed']);
+		\WP_Mock::expectFilterAdded('default_feed', [$useAtom, 'defaultFeed']);
 
-        \WP_Mock::wpFunction('remove_action', [
-            'args' => ['do_feed_rdf', 'do_feed_rdf', 10, 1],
-            'times' => 1,
-        ]);
-        \WP_Mock::wpFunction('remove_action', [
-            'args' => ['do_feed_rss', 'do_feed_rss', 10, 1],
-            'times' => 1,
-        ]);
-        \WP_Mock::wpFunction('remove_action', [
-            'args' => ['do_feed_rss2', 'do_feed_rss2', 10, 1],
-            'times' => 1,
-        ]);
+		\WP_Mock::wpFunction('remove_action', [
+			'args' => ['do_feed_rdf', 'do_feed_rdf', 10, 1],
+			'times' => 1,
+		]);
+		\WP_Mock::wpFunction('remove_action', [
+			'args' => ['do_feed_rss', 'do_feed_rss', 10, 1],
+			'times' => 1,
+		]);
+		\WP_Mock::wpFunction('remove_action', [
+			'args' => ['do_feed_rss2', 'do_feed_rss2', 10, 1],
+			'times' => 1,
+		]);
 
-        $useAtom->init();
+		$useAtom->init();
 
-        // Check that this code runs to completion.
-        $this->assertTrue(True);
-    }
+		// Check that this code runs to completion.
+		$this->assertTrue(true);
+	}
 
-    public function testDefaultFeed()
-    {
-        $useAtom = new \Dxw\Iguana\Extras\UseAtom();
+	public function testDefaultFeed()
+	{
+		$useAtom = new \Dxw\Iguana\Extras\UseAtom();
 
-        $this->assertEquals('atom', $useAtom->defaultFeed());
-    }
+		$this->assertEquals('atom', $useAtom->defaultFeed());
+	}
 
-    public function testWpHead()
-    {
-        $useAtom = new \Dxw\Iguana\Extras\UseAtom();
+	public function testWpHead()
+	{
+		$useAtom = new \Dxw\Iguana\Extras\UseAtom();
 
-        \WP_Mock::wpFunction('get_bloginfo', [
-            'args' => ['name'],
-            'return' => 'Xyz',
-        ]);
+		\WP_Mock::wpFunction('get_bloginfo', [
+			'args' => ['name'],
+			'return' => 'Xyz',
+		]);
 
-        \WP_Mock::wpFunction('esc_attr', [
-            'return' => function ($a) { return '_'.$a.'_'; },
-        ]);
+		\WP_Mock::wpFunction('esc_attr', [
+			'return' => function ($a) {
+				return '_'.$a.'_';
+			},
+		]);
 
-        \WP_Mock::wpFunction('get_feed_link', [
-            'args' => ['atom'],
-            'return' => 'xyz',
-        ]);
+		\WP_Mock::wpFunction('get_feed_link', [
+			'args' => ['atom'],
+			'return' => 'xyz',
+		]);
 
-        $this->expectOutputString('        <link rel="alternate" type="application/atom+xml" title="_Xyz_ Feed" href="_xyz_">'."\n        ");
+		$this->expectOutputString('        <link rel="alternate" type="application/atom+xml" title="_Xyz_ Feed" href="_xyz_">'."\n        ");
 
-        $useAtom->wpHead();
-    }
+		$useAtom->wpHead();
+	}
 }
